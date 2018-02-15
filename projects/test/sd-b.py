209b598ab2067ec501ba035839921e377be50e14
Atte JEnsen <atte.jensen@gmail.com>
2018-02-15 09:42:44 +0100

    no longer broken in b

--- projects/test/sd-b.py
+++ projects/test/sd-b.py
@@ -1,9 +1,3 @@
-print 'inside ' + self.file_name
-print 'part: ' + self.part
-print 'channel: ' + self.channel
-
-
 while self.running:
-    print 'looping ' + self.file_name
-    wait(1)
+    t.wait(1)
 
